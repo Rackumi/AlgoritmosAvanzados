f7c038d26be3c7e9ae687f7facb041efcbecf873
Rackumi <rackumi4@gmail.com>
2020-10-15 19:20:12 +0200

    Practica 1 explicación del código

--- src/Practica_1.java
+++ src/Practica_1.java
@@ -1,58 +1,70 @@
-
 public class Practica_1 {
 
     public static void main(String[] args) {
 
-        int a = tareasYaOrdenadas(new int[]{1, 2, 3, 4, 5}, 2);
+        int a = tareasIdeal(new int[]{1, 2, 3, 4, 5}, 2);
         System.out.println(a);
 
-        a = tareasSinOrdenar(new int[]{5, 3, 4, 1, 2}, 2);
+        a = tareasReal(new int[]{5, 3, 4, 1, 2}, 2);
         System.out.println(a);
+
     }
 
-    public static int tareasYaOrdenadas(int[] ts, int n) {
+    /**
+     * @param ts es un vector que contiene los tiempos de ejecución de las tareas
+     * @param n es el numero de procesadores disponibles
+     * @return el tiempo total de espera de todas las tareas
+     */
+    public static int tareasIdeal(int[] ts, int n) {
+        int duracionTotal = 0; //duracion total inicializada a 0
+        int[] procesadores = new int[n]; //declaramos e inicializamos el array de los procesadores de tamaño n
+        int procAct = 0; //empezamos con el primer procesador
 
-        int duracion = 0;
-        int[] procesadores = new int[n];
-        int procAct = 0;
+        for (int t : ts) { //recorremos el array de tareas con un for each
+            procesadores[procAct] += t; //calculamos el tiempo de espera de esa actividad
 
-        for (int t : ts) {
-            procesadores[procAct] += t;
+            duracionTotal += procesadores[procAct]; //vamos acumulando el tiempo de espera de cada actividad
 
-            duracion += procesadores[procAct];
-
-            if (procAct == n-1)
+            if (procAct == n-1) //con esta condición vamos cambiando de procesador
                 procAct = 0;
             else
                 procAct++;
         }
 
-        return duracion;
+        return duracionTotal; //finalmente devolvemos el resultado acumulado
     }
 
-    public static int tareasSinOrdenar(int[] ts, int n) {
-        int[] ts2 = ordenarIndices(ts);
+    /**
+     * @param ts es un vector que contiene los tiempos de ejecución de las tareas
+     * @param n es el numero de procesadores disponibles
+     * @return el tiempo total de espera de todas las tareas
+     */
+    public static int tareasReal(int[] ts, int n) {
+        int[] ts2 = ordenarIndices(ts); //ordenamos los indices del array con el metodo ordenarIndices
 
-        int duracion = 0;
-        int[] procesadores = new int[n];
-        int procAct = 0;
+        int duracionTotal = 0; //duracion total inicializada a 0
+        int[] procesadores = new int[n]; //declaramos e inicializamos el array de los procesadores de tamaño n
+        int procAct = 0; //empezamos con el primer procesador
 
+        for (int i : ts2) { //recorremos el array de tareas ordenado con un for each
+            procesadores[procAct] += ts[i]; //calculamos el tiempo de espera de esa actividad
 
-        for (int i : ts2) {
-            procesadores[procAct] += ts[i];
+            duracionTotal += procesadores[procAct]; //vamos acumulando el tiempo de espera de cada actividad
 
-            duracion += procesadores[procAct];
-
-            if (procAct == n-1)
+            if (procAct == n-1) //con esta condición vamos cambiando de procesador
                 procAct = 0;
             else
                 procAct++;
         }
 
-        return duracion;
+        return duracionTotal; //finalmente devolvemos el resultado acumulado
     }
 
-    private static int[] ordenarIndices (int[] v1) {
+    /**
+     * @param v1 es el array a ordenar
+     * @return el array ordenado
+     */
+    private static int[] ordenarIndices(int[] v1) {
         int[] v2 = new int[v1.length];
         v2[0] = 0;
         for (int i=1; i<v1.length; i++) {
